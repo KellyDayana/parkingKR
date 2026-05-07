70e9c079885a05202079534c362b1cd6f39267db
KellyDayana <kelly.chicaiza5049@utc.edu.ec>
2026-05-07 14:35:37 -0500

    creando la clase cliente

--- cobros/models.py
+++ cobros/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class cliente(models.Model):
+    id_cliente = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    identificacion = models.CharField(max_length=20, unique=True)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
